6d8783582fc399a75b51f4daa095860f508e3dba
Oleh <oifilonchuk@gmail.com>
2019-07-11 18:23:25 +0300

    add generating random items

--- App.js
+++ App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Nav from './src/Nav/nav'
 import Generator from "./src/Generator/generator"
+import ListItem from "./src/Generator/list-item"
 
 export default class App extends React.Component {
 
   state = {
-    appName: 'My awesome app'
+    appName: 'My awesome app',
+    random: [20,13]
   }
 
   onAddRandom = () => {
-    alert('add random')
+    const random = Math.floor(Math.random()*100) + 1
+    this.setState(prevState =>  {
+      return {
+        random: [...prevState.random, random]
+      }
+    })
   }
 
   render() {
@@ -19,6 +26,7 @@ export default class App extends React.Component {
         <Nav appName={this.state.appName}/>
         <Text>{'some text just for fun'}</Text>
         <Generator add={this.onAddRandom}/>
+        <ListItem items={this.state.random}/>
       </View>
     )
   }
---
+++ src/Generator/list-item.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const ListItem = (props) => {
+  return (
+    props.items.map((item, i) => (
+      <TouchableOpacity
+        onPress={()=>alert('delete')}
+        key={i}
+        style={styles.listItem}
+      >
+        <View>
+          <Text>{item}</Text>
+        </View>
+      </TouchableOpacity>
+    ))
+  )
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    padding: 10,
+    width: '100%',
+    margin: 5
+  }
+})
+
+export default ListItem
